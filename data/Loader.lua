56a9e7ee192ecae0c4f130ac09c4fc10442ab2e5
PxnifyDev <Wusel_16_07@t-online.de>
2025-06-27 18:19:44 +0200

    Update Loader.lua mit neuer Job-ID

--- data/Loader.lua
+++ data/Loader.lua
@@ -1,1 +1,1 @@
-game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "d72832e6-9d39-4d7c-8ad9-a8c999d531b3")
+game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "d52bd290-1ed9-44fc-8502-f578d3e669ad")

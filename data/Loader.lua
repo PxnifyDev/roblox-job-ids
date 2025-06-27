47e675f3946a85f6c0f153ae48dff345dbb674a6
PxnifyDev <Wusel_16_07@t-online.de>
2025-06-27 18:38:13 +0200

    Update Job ID to fc89e8b5-751a-430b-9434-462a56b4eaed

--- data/Loader.lua
+++ data/Loader.lua
@@ -1,2 +1,2 @@
-EggTimeLeft = 300
-game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "fd813d14-1f6c-424a-8a8a-1bacb2cfd2d3")
+EggTimeLeft = 600
+game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "fc89e8b5-751a-430b-9434-462a56b4eaed")

b3acbac529f1e63e6fbb3f6d211661b8a0d0b7ac
PxnifyDev <Wusel_16_07@t-online.de>
2025-06-27 18:40:21 +0200

    Update Job ID to 4303139d-f7e4-4668-bbb9-78a0b3038c6a

--- data/Loader.lua
+++ data/Loader.lua
@@ -1,2 +1,2 @@
-EggTimeLeft = 420
-game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "50c8a67f-5828-4e17-989c-e884c9a9c311")
+EggTimeLeft = 540
+game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "4303139d-f7e4-4668-bbb9-78a0b3038c6a")

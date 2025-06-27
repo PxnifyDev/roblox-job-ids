61fe6639766042f6d88eaf43ffbb7964ae389feb
PxnifyDev <Wusel_16_07@t-online.de>
2025-06-27 19:00:03 +0200

    Update Loader.lua

--- data/Loader.lua
+++ data/Loader.lua
@@ -1,5 +1,5 @@
 -- Zeit in Sekunden, wie lange das Egg verfügbar ist
-local EggTimeLeft = 600
+local EggTimeLeft = 30
 
 while true do
     game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "6213705a-1ca1-48af-a1f4-b47aa36e49d2")

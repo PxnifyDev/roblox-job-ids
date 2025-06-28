21de5e9b7e2b699b98f3e098014c3be4ca845107
PxnifyDev <Wusel_16_07@t-online.de>
2025-06-28 12:41:55 +0200

    update Loader.lua with new job id

--- data/Loader.lua
+++ data/Loader.lua
@@ -1,5 +1,5 @@
 -- Automatisch generiert
 while true do
-    game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "578b38b4-eaa0-4ca1-a688-dc1ca84c4f4e")
+    game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "67e31659-41c8-437d-a38b-070736279e82")
     break
 end

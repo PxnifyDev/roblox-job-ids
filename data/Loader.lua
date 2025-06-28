d612970d2ecdc0bd9c6862f5a7c64a9d58d047a3
PxnifyDev <Wusel_16_07@t-online.de>
2025-06-28 11:26:00 +0200

    Update Loader.lua mit neuer Job-ID

--- data/Loader.lua
+++ data/Loader.lua
@@ -1,5 +1,5 @@
--- Roblox Teleport Script
-local EggTimeLeft = 590
+-- Zeit in Sekunden, wie lange das Egg verfügbar ist
+local EggTimeLeft = 600
 while true do
     game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "cf0548d1-03e1-4052-8e17-11d994f319c2")
     wait(EggTimeLeft)

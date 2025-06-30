6cef346b1a8918ae012dd257f24f8a10fff51ced
PxnifyDev <Wusel_16_07@t-online.de>
2025-06-30 05:39:35 +0200

    Update Time.lua

--- data/Time.lua
+++ data/Time.lua
@@ -1,3 +1,6 @@
 local EggTime = 90
-wait(EggTime)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PxnifyDev/roblox-job-ids/main/data/Loader.lua"))()
+
+while true do
+    wait(EggTime)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PxnifyDev/roblox-job-ids/main/data/Loader.lua"))()
+end

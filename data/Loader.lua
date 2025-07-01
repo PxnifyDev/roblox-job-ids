bf95ec04d1e16ac98faefed339a0f6ca3154ce3a
PxnifyDev <Wusel_16_07@t-online.de>
2025-07-01 10:51:43 +0200

    Update Loader.lua with Job ID

--- data/Loader.lua
+++ data/Loader.lua
@@ -1,7 +1,7 @@
 -- Zeit in Sekunden
 local EggTimeLeft = 540
 while true do
-    game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "9b7f4ea6-0e6f-4886-8b40-b24037c3b770")
+    game:GetService("TeleportService"):TeleportToPlaceInstance(85896571713843, "b1aa20be-91a1-45a6-94c6-64c800194fc6")
     wait(EggTimeLeft)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/PxnifyDev/roblox-job-ids/main/data/Loader.lua"))()
 end
